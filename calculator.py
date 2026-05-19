fa24a2b8aaa7c55da85a99f4f75b47b3b688cac9
muradnasirov63-droid <muradnasirov63@gmail.com>
2026-05-19 15:58:43 +0300

    change divide to raise ZeroDivisionError

--- calculator.py
+++ calculator.py
@@ -9,7 +9,7 @@ def multiply(a, b):
 
 def divide(a, b):
     if b == 0:
-        raise ValueError("Cannot divide by zero")
+        raise ZeroDivisionError("Division by zero is forbidden")
     return a / b
 
 def power(a, b):
